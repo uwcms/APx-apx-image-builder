3e34f007b422dce1f7b748587ae57e0c0326e458
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2021-08-04 11:17:55 -0400

    Rename uboot to match its canonical naming.

--- apx-image-builder/builders/rootfs.py
+++ apx-image-builder/builders/rootfs.py
@@ -8,24 +8,22 @@ from typing import List, Optional
 from . import base
 
 
-class UBootBuilder(base.BaseBuilder):
-	NAME: str = 'u-boot'
+class RootfsBuilder(base.BaseBuilder):
+	NAME: str = 'rootfs'
 	statefile: Optional[base.JSONStateFile] = None
 	makeflags: List[str]
 
 	@classmethod
 	def prepare_argparse(cls, group: argparse._ArgumentGroup) -> None:
 		group.description = '''
-Build U-Boot
+Build rootfs using buildroot.
 
 Stages available:
-  fetch: Download or copy sources.
+  fetch: Download or copy buildroot sources.
   prepare: Extract sources.
-  (defconfig): Run `make xilinx_zynq_virt_defconfig` or
-               Run `make xilinx_zynqmp_virt_defconfig`
-               as appropriate to your zynq_series configuration.
+  (defconfig): Run `make defconfig`.
   (nconfig): Run `make nconfig`
-  build: Build U-Boot
+  build: Build the rootfs
 '''.strip()
 
 	def instantiate_stages(self) -> None:
@@ -61,22 +59,9 @@ Stages available:
 
 		check_ok: bool = True
 		if STAGE.name in ('fetch', 'prepare'
-		                  ) and 'uboot_tag' not in self.BUILDER_CONFIG and 'uboot_sourceurl' not in self.BUILDER_CONFIG:
+		                  ) and 'buildroot_version' not in self.BUILDER_CONFIG and 'buildroot_sourceurl' not in self.BUILDER_CONFIG:
 			LOGGER.error(
-			    'Please set a `uboot_tag` or `uboot_sourceurl` (file://... is valid) in the configuration for the "u-boot" builder.'
-			)
-			check_ok = False
-		if self.COMMON_CONFIG['zynq_series'] == 'zynq':
-			self.cross_compile = 'arm-none-eabi-'
-		elif self.COMMON_CONFIG['zynq_series'] == 'zynqmp':
-			self.cross_compile = 'aarch64-none-elf-'
-		if 'cross_compile' in self.BUILDER_CONFIG:
-			self.cross_compile = self.BUILDER_CONFIG['cross_compile']
-		if not shutil.which(self.cross_compile + 'gcc'):
-			LOGGER.error(
-			    'Unable to locate `{cross_compile}gcc`.  Did you source the Vivado environment files?'.format(
-			        cross_compile=self.cross_compile
-			    )
+			    'Please set a `buildroot_version` or `buildroot_sourceurl` (file://... is valid) in the configuration for the "rootfs" builder.'
 			)
 			check_ok = False
 		return check_ok
@@ -86,12 +71,12 @@ Stages available:
 			return  # We're bypassed.
 
 		statefile = base.JSONStateFile(PATHS.build / '.state.json')
-		sourceurl: Optional[str] = self.BUILDER_CONFIG.get('uboot_sourceurl', None)
+		sourceurl: Optional[str] = self.BUILDER_CONFIG.get('buildroot_sourceurl', None)
 		if sourceurl is None:
-			sourceurl = 'https://github.com/Xilinx/u-boot-xlnx/archive/refs/tags/{tag}.tar.gz'.format(
-			    tag=self.BUILDER_CONFIG['uboot_tag']
+			sourceurl = 'https://buildroot.org/downloads/buildroot-{version}.tar.gz'.format(
+			    version=self.BUILDER_CONFIG['buildroot_version']
 			)
-		if base.import_source(PATHS, LOGGER, self.ARGS, sourceurl, 'u-boot.tar.gz'):
+		if base.import_source(PATHS, LOGGER, self.ARGS, sourceurl, 'buildroot.tar.gz'):
 			with statefile as state:
 				state['tree_ready'] = False
 
@@ -100,22 +85,21 @@ Stages available:
 			return  # We're bypassed.
 
 		statefile = base.JSONStateFile(PATHS.build / '.state.json')
-		ubdir = PATHS.build / 'u-boot'
+		ubdir = PATHS.build / 'buildroot'
 		patcher = base.Patcher(PATHS.build / 'patches')
-		patches = ['builtin:///uboot_data/01_config_user.patch']
-		patches.extend(self.BUILDER_CONFIG.get('patches', []))
+		patches = self.BUILDER_CONFIG.get('patches', [])
 		if patcher.import_patches(PATHS, LOGGER, self.ARGS, patches):
 			with statefile as state:
 				state['tree_ready'] = False
 		if statefile.state.get('tree_ready', False):
-			LOGGER.info('The U-Boot source tree has already been extracted.  Skipping.')
+			LOGGER.info('The buildroot source tree has already been extracted.  Skipping.')
 		else:
-			base.untar(PATHS, LOGGER, PATHS.build / 'u-boot.tar.gz', PATHS.build / 'u-boot')
-			patcher.apply(PATHS, LOGGER, PATHS.build / 'u-boot')
+			base.untar(PATHS, LOGGER, PATHS.build / 'buildroot.tar.gz', PATHS.build / 'buildroot')
+			patcher.apply(PATHS, LOGGER, PATHS.build / 'buildroot')
 			with statefile as state:
 				state['tree_ready'] = True
 
-		if base.import_source(PATHS, LOGGER, self.ARGS, 'u-boot.config', PATHS.build / '.config',
+		if base.import_source(PATHS, LOGGER, self.ARGS, 'rootfs.config', PATHS.build / '.config',
 		                      ignore_timestamps=True):
 			# We need to use a two stage load here because we actually do update
 			# the imported source, and don't want needless imports to interfere
@@ -126,20 +110,8 @@ Stages available:
 				with statefile as state:
 					state['user_config_hash'] = user_config_hash
 
-		if base.import_source(PATHS, LOGGER, self.ARGS, 'u-boot.config_user.h', ubdir / 'include/config_user.h'):
-			# Since we artificially created this include with a patch, I'm not
-			# fully confident that Make will appropriately propogate updates to
-			# these files when doing its freshness detection.
-			# Let's touch the files that #include it.
-			base.run(
-			    PATHS, LOGGER, [
-			        'touch', '--no-create', ubdir / 'include/configs/xilinx_zynqmp.h',
-			        ubdir / 'include/configs/zynq-common.h'
-			    ]
-			)
-
 		# Provide our config as an output.
-		shutil.copyfile(ubdir / '.config', PATHS.output / 'u-boot.config')
+		shutil.copyfile(ubdir / '.config', PATHS.output / 'rootfs.config')
 
 	def defconfig(self, STAGE: base.Stage, PATHS: base.BuildPaths, LOGGER: logging.Logger) -> None:
 		if base.check_bypass(STAGE, PATHS, LOGGER):
@@ -153,28 +125,28 @@ Stages available:
 		else:
 			base.fail(LOGGER, "Unknown zynq_series setting: " + repr(zynq_series))
 
-		ubdir = PATHS.build / 'u-boot'
+		ubdir = PATHS.build / 'rootfs'
 		LOGGER.info('Running `{defconfig}`...'.format(defconfig=defconfig))
 		try:
 			base.run(PATHS, LOGGER, ['make', 'CROSS_COMPILE=' + self.cross_compile, defconfig], cwd=ubdir)
 		except subprocess.CalledProcessError:
-			base.fail(LOGGER, 'U-Boot `{defconfig}` returned with an error'.format(defconfig=defconfig))
+			base.fail(LOGGER, 'rootfs `{defconfig}` returned with an error'.format(defconfig=defconfig))
 		LOGGER.info('Finished `{defconfig}`.'.format(defconfig=defconfig))
 
 		# Provide our kernel config as an output.
-		shutil.copyfile(ubdir / '.config', PATHS.output / 'u-boot.config')
+		shutil.copyfile(ubdir / '.config', PATHS.output / 'rootfs.config')
 		LOGGER.warning(
-		    'The output file `u-boot.config` has been created.  You must manually copy this to your sources directory.'
+		    'The output file `rootfs.config` has been created.  You must manually copy this to your sources directory.'
 		)
 
 	def nconfig(self, STAGE: base.Stage, PATHS: base.BuildPaths, LOGGER: logging.Logger) -> None:
 		if base.check_bypass(STAGE, PATHS, LOGGER):
 			return  # We're bypassed.
 
-		ubdir = PATHS.build / 'u-boot'
+		ubdir = PATHS.build / 'rootfs'
 
 		if not (ubdir / '.config').exists():
-			base.fail(LOGGER, 'No U-Boot configuration file was found.  Use u-boot:defconfig to generate one.')
+			base.fail(LOGGER, 'No rootfs configuration file was found.  Use rootfs:defconfig to generate one.')
 
 		LOGGER.info('Running `nconfig`...')
 		try:
@@ -187,20 +159,20 @@ Stages available:
 			    stderr=None
 			)
 		except subprocess.CalledProcessError:
-			base.fail(LOGGER, 'U-Boot `nconfig` returned with an error')
+			base.fail(LOGGER, 'rootfs `nconfig` returned with an error')
 		LOGGER.info('Finished `nconfig`.')
 
 		# Provide our kernel config as an output.
-		shutil.copyfile(ubdir / '.config', PATHS.output / 'u-boot.config')
+		shutil.copyfile(ubdir / '.config', PATHS.output / 'rootfs.config')
 		LOGGER.warning(
-		    'The output file `u-boot.config` has been created.  You must manually copy this to your sources directory.'
+		    'The output file `rootfs.config` has been created.  You must manually copy this to your sources directory.'
 		)
 
 	def build(self, STAGE: base.Stage, PATHS: base.BuildPaths, LOGGER: logging.Logger) -> None:
 		if base.check_bypass(STAGE, PATHS, LOGGER):
 			return  # We're bypassed.
 
-		ubdir = PATHS.build / 'u-boot'
+		ubdir = PATHS.build / 'buildroot'
 		LOGGER.info('Running `make`...')
 		try:
 			base.run(PATHS, LOGGER, ['make', 'CROSS_COMPILE=' + self.cross_compile], cwd=ubdir)
