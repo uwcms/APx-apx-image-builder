bfaa0626b482da528ff70d4146de827c29b4a2dc
Jesra Tikalsky <jtikalsky@hep.wisc.edu>
2021-08-25 12:57:05 -0400

    Missed one, reordering JTAG/QSPI

--- apx-image-builder/builders/__init__.py
+++ apx-image-builder/builders/__init__.py
@@ -6,8 +6,8 @@ from .dtb import DTBBuilder
 from .uboot import UBootBuilder
 from .kernel import KernelBuilder
 from .rootfs import RootfsBuilder
-from .qspi import QSPIBuilder
 from .jtag import JTAGBuilder
+from .qspi import QSPIBuilder
 
 # Keep this list in dependency order as much as possible, to minimize the amount
 # of intermixing of steps between builders that results from dependency
